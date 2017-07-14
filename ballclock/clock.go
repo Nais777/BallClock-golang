528ba2541dfa2876a1f7bab34e4ad7a179bcadb2
Jacob Lambert <jacob.lambert@clearlink.com>
2017-07-13 23:33:59 -0600

    Removed not needed structs

--- ballclock/clock.go
+++ ballclock/clock.go
@@ -16,72 +16,102 @@ const fiveAndHourTrackCap int = 11
 
 //Clock is an instance of a ballclock
 type Clock struct {
-	timeTracks []*timeTrack
-	ballQueue  *queueTrack
+	timeTracks [][]int
+	ballQueue  []int
+	ballCount  int
 }
 
 //NewClock returns a new instance of the clock type
-func NewClock(ballCount int) (c *Clock, err error) {
+func NewClock(ballCount int) (*Clock, error) {
 	if ballCount < MinBalls || ballCount > MaxBalls {
 		return nil, fmt.Errorf("Invalid ball count. %v provided, must be between %v and %v", ballCount, MinBalls, MaxBalls)
 	}
 
-	return &Clock{
-		ballQueue: newQueueTrack(ballCount),
-		timeTracks: []*timeTrack{
-			newTimeTrack(minTrackCap),
-			newTimeTrack(fiveAndHourTrackCap),
-			newTimeTrack(fiveAndHourTrackCap)},
-	}, nil
+	c := &Clock{
+		ballQueue: make([]int, ballCount, ballCount),
+		timeTracks: [][]int{
+			make([]int, 0, minTrackCap),
+			make([]int, 0, fiveAndHourTrackCap),
+			make([]int, 0, fiveAndHourTrackCap)},
+		ballCount: ballCount,
+	}
+
+	for i := 0; i < ballCount; i++ {
+		c.ballQueue[i] = i
+	}
+
+	return c, nil
+
 }
 
 //GetTrackState returns a State object depicting the internal ball structure of the Clock
 func (c *Clock) GetTrackState() *State {
 	return &State{
-		Min:     c.timeTracks[0].getContentIds(),
-		FiveMin: c.timeTracks[1].getContentIds(),
-		Hour:    c.timeTracks[2].getContentIds(),
-		Main:    c.ballQueue.getContentIds(),
+		Min:     c.timeTracks[0],
+		FiveMin: c.timeTracks[1],
+		Hour:    c.timeTracks[2],
+		Main:    c.ballQueue,
 	}
 }
 
 //IsOriginalConfig returns true or false indicating if the Clock is in the same state immediatly after creation.
 func (c *Clock) IsOriginalConfig() bool {
-	return c.ballQueue.isOriginalConfig()
+	if len(c.ballQueue) != c.ballCount {
+		return false
+	}
+
+	for i := 0; i < c.ballCount; i++ {
+		if c.ballQueue[i] != i {
+			return false
+		}
+	}
+
+	return true
 }
 
 //Tick increments the Clock by one minute
 func (c *Clock) Tick() {
-	b := c.ballQueue.getBall()
+	var b int
+	b, c.ballQueue = c.ballQueue[0], c.ballQueue[1:]
 
-	for t := 0; t < 3; t++ {
-		overFlow := c.timeTracks[t].increment(b)
-		if overFlow == nil {
+	for i := 0; i < 3; i++ {
+		var o []int
+		o, c.timeTracks[i] = addOrOverflow(c.timeTracks[i], b)
+		if o == nil {
 			return
 		}
 
-		c.ballQueue.returnBalls(overFlow)
+		c.ballQueue = append(c.ballQueue, o...)
 	}
 
-	c.ballQueue.returnBall(b)
+	c.ballQueue = append(c.ballQueue, b)
 }
 
 //TickFive causes the clock to tick 5 minutes
 func (c *Clock) TickFive() {
-	o, b, balls := c.ballQueue.balls[0:4], c.ballQueue.balls[4], c.ballQueue.balls[5:]
-	c.ballQueue.balls = balls
-	c.ballQueue.returnBalls(reverseSlice(o))
+	var b int
 
-	for t := 1; t < 3; t++ {
-		overFlow := c.timeTracks[t].increment(b)
-		if overFlow == nil {
+	b, c.ballQueue = c.ballQueue[4], append(c.ballQueue[5:], reverseSlice(c.ballQueue[0:4])...)
+
+	for i := 1; i < 3; i++ {
+		var o []int
+		o, c.timeTracks[i] = addOrOverflow(c.timeTracks[i], b)
+		if o == nil {
 			return
 		}
 
-		c.ballQueue.returnBalls(overFlow)
+		c.ballQueue = append(c.ballQueue, o...)
 	}
 
-	c.ballQueue.returnBall(b)
+	c.ballQueue = append(c.ballQueue, b)
+}
+
+func addOrOverflow(s []int, b int) (overflow []int, slice []int) {
+	if len(s) != cap(s) {
+		return nil, append(s, b)
+	}
+
+	return reverseSlice(s), s[:0]
 }
 
 func reverseSlice(s []int) []int {
